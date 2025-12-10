becc7d86eb586b5af9e6f8ec48e7490dd087a1f5
Cindy Wu <cindyw44@nycstudents.net>
2025-12-10 14:14:48 -0500

    done

--- activity-5.js
+++ activity-5.js
@@ -8,7 +8,12 @@ function createMultiplier(factor) {
     };
 }
 // Convert to arrow function (convert both outer and inner functions):
-
+const createMultiplier2 = (factor) => {
+    return function(number) {
+        return number * factor;
+    };
+}
+console.log(createMultiplier2);
 
 // Problem 2
 function processArray(arr, callback) {
@@ -17,7 +22,12 @@ function processArray(arr, callback) {
     });
 }
 // Convert to arrow function (convert both outer and inner functions):
-
+const processArray2 = (arr, callback) => {
+    return arr.map(function(item) {
+        return callback(item);
+    });
+}
+console.log(processArray2);
 
 // Problem 3
 function createCounter() {
@@ -28,7 +38,14 @@ function createCounter() {
     };
 }
 // Convert to arrow function (convert both outer and inner functions):
-
+const createCounter2 = () => {
+    let count = 0;
+    return function() {
+        count++;
+        return count;
+    };
+}
+console.log(createCounter2);
 
 // Problem 4
 function sortByProperty(objects, property) {
@@ -37,7 +54,12 @@ function sortByProperty(objects, property) {
     });
 }
 // Convert to arrow function (convert both outer and inner functions):
-
+const sortByProperty2 = (objects, property) => {
+    return objects.sort(function(a, b) {
+        return a[property] - b[property];
+    });
+}
+console.log(sortByProperty2);
 
 // Problem 5
 function filterAndMap(numbers) {
@@ -50,7 +72,16 @@ function filterAndMap(numbers) {
         });
 }
 // Convert to arrow function (convert all functions):
-
+const filterAndMap2 = (numbers) => {
+    return numbers
+        .filter(function(num) {
+            return num > 0;
+        })
+        .map(function(num) {
+            return num * 2;
+        });
+}
+console.log(filterAndMap2);
 
 // Problem 6
 function createValidator(min, max) {
@@ -59,7 +90,12 @@ function createValidator(min, max) {
     };
 }
 // Convert to arrow function (convert both outer and inner functions):
-
+const createValidator2 = (min, max) => {
+    return function(value) {
+        return value >= min && value <= max;
+    };
+}
+console.log(createValidator2);
 
 // Problem 7
 function processData(data, transform) {
@@ -69,7 +105,13 @@ function processData(data, transform) {
     }, []);
 }
 // Convert to arrow function (convert both outer and inner functions):
-
+const processData2 = (data, transform) => {
+    return data.reduce(function(acc, item) {
+        acc.push(transform(item));
+        return acc;
+    }, []);
+}
+console.log(processData2);
 
 // Problem 8
 function createAdder(x) {
@@ -80,7 +122,14 @@ function createAdder(x) {
     };
 }
 // Convert to arrow function (convert all nested functions):
-
+const createAdder2 = (x) => {
+    return function(y) {
+        return function(z) {
+            return x + y + z;
+        };
+    };
+}
+console.log(createAdder2);
 
 // Problem 9
 function groupBy(array, keyFn) {
@@ -94,7 +143,17 @@ function groupBy(array, keyFn) {
     }, {});
 }
 // Convert to arrow function (convert both outer and inner functions):
-
+const groupBy2 = (array, keyFn) => {
+    return array.reduce(function(groups, item) {
+        const key = keyFn(item);
+        if (!groups[key]) {
+            groups[key] = [];
+        }
+        groups[key].push(item);
+        return groups;
+    }, {});
+}
+console.log(groupBy2);
 
 // Problem 10
 function compose(f, g) {
@@ -103,4 +162,9 @@ function compose(f, g) {
     };
 }
 // Convert to arrow function (convert both outer and inner functions):
-
+const compose2 = (f, g) => {
+    return function(x) {
+        return f(g(x));
+    };
+}
+console.log(compose2);
